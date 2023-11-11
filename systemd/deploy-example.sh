3063aa0fb18fcb50d453358067f4673b1cf2b15e
Joshua Schmidlkofer <joshua@joshuainnovates.us>
2023-11-11 12:14:41 -0800

    Update: deployment example script

--- systemd/deploy-example.sh
+++ systemd/deploy-example.sh
@@ -16,7 +16,7 @@ for user in ${*}; do
     cp -f Path.service deploy/${user}.service
     cp -f Path.path deploy/${user}.path
     sed -i "s/TARGET/${user}/g" deploy/${user}.service deploy/${user}.path
+    sed -i "s:/path/to/notices:/srv/webhook/notice:g" deploy/${user}.service deploy/${user}.path
+    sed -i "s:/path/to/WorkingDirectory:/blog/${user}:g" deploy/${user}.service deploy/${user}.path
+    sed -i "s:/path/to/pipelines:/srv/pipelines:g" deploy/${user}.service deploy/${user}.path
     done
-    
-
-    
